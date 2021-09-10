828314d9b9c2fbb951ed316a53007928e27d2084
Tomas Bolio <tomas_9724@hotmail.com>
2021-09-09 21:22:13 -0500

    todos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useRef, useEffect} from 'react';
+import {v4 as uuidv4} from 'uuid';
+import ReactDom from 'react-dom';
+import {TodoList} from './components/TodoList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const KEY = 'todoApp.todos'
+
+export function App() {
+  const [todos, setTodos] = useState([
+    {id:1, task:'Tarea1', completed: false}]);
+
+    const todoTaskRef = useRef();
+
+    useEffect(() => {
+      const storedTodos = JSON.parse(localStorage.getItem(KEY));
+      if(storedTodos ){
+        setTodos(storedTodos)
+      }
+    }, [])
+
+    useEffect(()=> {
+      localStorage.setItem(KEY, JSON.stringify(todos))
+    }, [todos])
+
+    const toggleTodo = (id) => {
+      const newTodos = [...todos];
+      const todo = newTodos.find(todo => todo.id === id);
+      todo.completed =! todo.completed;
+      setTodos(newTodos);
+    }
+
+    const handleTodoAdd = () => {
+      const task = todoTaskRef.current.value;
+      if(task === '') return;
+      setTodos((prevTodos) => {
+        return[...prevTodos,{id: uuidv4(), task,completed:false}]
+      })
+      todoTaskRef.current.value=null;
+    }
+
+    const handleClearAll = () => {
+      const newTodos = todos.filter(todo => !todo.completed)
+      setTodos(newTodos);
+    }
+
+    return(
+    <>
+      <TodoList todos={todos} toggleTodo={toggleTodo}/>
+      <input ref={todoTaskRef} type="text" placeholder="New Task" />
+      <button onClick={handleTodoAdd}>+</button>
+      <button onClick={handleClearAll}>-</button>
+      <div>Te quedan {todos.filter(todo => !todo.completed).length}  tareas por terminar</div>
+    </>
   );
 }
-
-export default App;
